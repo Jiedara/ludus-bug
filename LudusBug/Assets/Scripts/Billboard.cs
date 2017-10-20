2b0bc45343d99f58c5c946c9d471edea6ec6f889
no <noemie.scherer@gmail.com>
2017-10-20 23:54:39 +0200

    player has flare

---
+++ LudusBug/Assets/Scripts/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+    private GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward,
+        camera.transform.rotation * Vector3.up);
+    }
+}
