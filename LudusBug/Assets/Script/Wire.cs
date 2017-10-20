da3abf286ff4cc468f5535673b7f1c0bb50ad786
Antonin <antoriche.ar@gmail.com>
2017-10-21 01:41:50 +0200

    add wire component to load scene

---
+++ LudusBug/Assets/Script/Wire.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wire : MonoBehaviour {
+
+	//public Scene scene;
+	const string folder = "Scene/";
+	public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.CompareTag ("Player")) {
+			//SceneManager.SetActiveScene (scene);
+			SceneManager.LoadScene(folder+sceneName);
+		}
+	}
+}
