319f6702b509070df71252ed42d92aab4127beb3
Antonin <antoriche.ar@gmail.com>
2017-10-20 22:21:47 +0200

    add diode prefab

---
+++ LudusBug/Assets/Scripts/Diode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diode : MonoBehaviour {
+
+	//[SerializeField]
+	Transform output;
+
+	// Use this for initialization
+	void Start () {
+		output = gameObject.transform.GetChild (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		col.gameObject.transform.position = output.transform.position;
+	}
+}
