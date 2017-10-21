8848021717a600a7fd89111f26c20a71c12f20c0
no <noemie.scherer@gmail.com>
2017-10-22 00:16:03 +0200

    BGsphere, sol et plafond, bigger colliders dans tutos -still useless tho

---
+++ LudusBug/Assets/Script/flicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flicker : MonoBehaviour {
+    Renderer rd;
+    IEnumerator changeGoalNow;
+    float goal;
+    float changeIntensity;
+    float steps = .1f;
+    float change;
+    Vector2 wait = new Vector2(.1f,1);
+
+	// Use this for initialization
+	void Start () {
+        rd = GetComponent<Renderer>();
+        changeGoalNow = changeGoal(wait.x, wait.y);
+        change = transform.localScale.x;
+	}
+	
+    IEnumerator changeGoal(float waitMin, float waitMax)
+    {
+        goal = Random.Range(0, changeIntensity);
+        yield return new WaitForSeconds(Random.Range(waitMin, waitMax));
+        StartCoroutine(changeGoalNow);
+    }
+
+
+
+	// Update is called once per frame
+	void Update () {
+        change = Mathf.Lerp(transform.localScale.x, goal, goal-change/20);
+        transform.localScale += new Vector3(change,change,change);
+        rd.material.SetColor("_EmissionColor", new Color(change, change, change, 1));
+	}
+}
