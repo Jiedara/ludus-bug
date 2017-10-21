7cb6f223b6d9ae7ee094e05f8af5ca6823378777
Antonin <antoriche.ar@gmail.com>
2017-10-21 11:40:07 +0200

    Add capacitor component

---
+++ LudusBug/Assets/Script/Capacitor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capacitor : MonoBehaviour {
+		
+	[Range(0,100)]
+	public float regenerationSpeed;
+
+	PlayerEnergy player;
+
+
+
+	void OnTriggerEnter(Collider col){
+		print ("player enter in capacitor area");
+		player = col.gameObject.GetComponent<PlayerEnergy> ();
+	}
+	void OnTriggerExit(Collider col){
+		print ("player exit capacitor area");
+		player = null;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (player) {
+			player.RestoreEnergy (regenerationSpeed*Time.deltaTime);
+		}
+	}
+}
--- LudusBug/Assets/Script/PlayerEnergy.cs
+++ LudusBug/Assets/Script/PlayerEnergy.cs
@@ -13,6 +13,11 @@ public class PlayerEnergy : MonoBehaviour {
     [SerializeField]
     float CurrEnergy;
 
+	[SerializeField]
+	Color lowestColor;
+	[SerializeField]
+	Color highestColor;
+
 	// Use this for initialization
 	void Start () {
         CurrEnergy = StartEnergy;
@@ -39,5 +44,21 @@ public class PlayerEnergy : MonoBehaviour {
 
     void Update () {
         CurrEnergy = CurrEnergy - Time.deltaTime * EnergyConsumptionSpeed;
+		CalcMaterial ();
+	}
+
+	public void RestoreEnergy(float amount){
+		CurrEnergy += amount;
+		CurrEnergy = Mathf.Clamp (CurrEnergy, -1, StartEnergy);
+	}
+
+	private void CalcMaterial(){
+		float ratio = CurrEnergy / StartEnergy;
+		float r = lowestColor.r * (1 - ratio) + highestColor.r * ratio;
+		float g = lowestColor.g * (1 - ratio) + highestColor.g * ratio;
+		float b = lowestColor.b * (1 - ratio) + highestColor.b * ratio;
+		float a = lowestColor.a * (1 - ratio) + highestColor.a * ratio;
+		Color playerColor = new Color(r,g,b,a);
+		this.gameObject.GetComponentInChildren<Renderer>().material.color = playerColor;
 	}
 }
