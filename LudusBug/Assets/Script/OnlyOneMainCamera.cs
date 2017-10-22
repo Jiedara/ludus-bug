ada28822a121c538a93f213fbec9cd27965b69d8
no <noemie.scherer@gmail.com>
2017-10-22 13:14:40 +0200

    only one camera script

---
+++ LudusBug/Assets/Script/OnlyOneMainCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyOneMainCamera : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        if (GameManager.MainCamera == null)
+        {
+            GameManager.MainCamera = gameObject;
+            DontDestroyOnLoad(this);
+        }
+        else if (GameManager.MainCamera != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
